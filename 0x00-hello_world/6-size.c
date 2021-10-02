9c477c177825fbe543d2fbaa693091e76a192404
xARKEINx <3884@holbertonschool.com>
2021-10-01 18:45:44 -0700

    duro

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - sizeof
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size a char: %ld byte(s)", sizeof(char));
+	printf("size an int: %ld byte(s)", sizeof(int));
+	printf("size a long int: %ld byte(s)", sizeof(long int));
+	printf("size a long long int: %ld byte(s)", sizeof(long long int));
+	printf("size a float: %ld byte(s)", sizeof(float))
+		return (0)
+}
