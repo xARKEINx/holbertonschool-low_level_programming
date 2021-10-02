a55884698df5b37955ed7d9187dcb4e278e53917
xARKEINx <3884@holbertonschool.com>
2021-10-01 19:18:41 -0700

    luffy

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - sizeof
  * Return: 0
  */
 int main(void)
 {
-	printf("size a char: %ld byte(s)", sizeof(char));
-	printf("size an int: %ld byte(s)", sizeof(int));
-	printf("size a long int: %ld byte(s)", sizeof(long int));
-	printf("size a long long int: %ld byte(s)", sizeof(long long int));
-	printf("size a float: %ld byte(s)", sizeof(float))
-		return (0)
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
+
+	return (0);
 }
