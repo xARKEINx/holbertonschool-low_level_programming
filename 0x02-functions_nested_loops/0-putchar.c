f9630f121676145fb45469503b6e919c7e85ef59
xARKEINx <3884@holbertonschool.com>
2021-10-10 15:00:14 -0700

    ptos

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - loops
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int d;
+	for (d = 0; d <= 7; d++)
+	{
+		_putchar(c[d]);
+	}
+	_putchar('\n');
+	return (0);
+}
