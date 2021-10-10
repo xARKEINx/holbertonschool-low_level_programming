78155888d67d22a819b7765784a6fbf07da9eae0
xARKEINx <3884@holbertonschool.com>
2021-10-10 16:20:34 -0700

    ty

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+		_putchar(a);
+	_putchar('\n');
+}
