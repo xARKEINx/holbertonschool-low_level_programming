8fc7cd8cfec81b74965535bc9b65e2e6fcfd3026
xARKEINx <3884@holbertonschool.com>
2021-10-12 13:29:46 -0700

    put

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print Number
+ * a: Variable
+ * Return: numbers
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
