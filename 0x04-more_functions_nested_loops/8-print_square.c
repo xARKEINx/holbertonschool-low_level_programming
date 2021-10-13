4410c33750bd357afea5c9382cb7c1c8fd23c47a
xARKEINx <3884@holbertonschool.com>
2021-10-13 13:41:08 -0700

    numeral

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  * print_square - Entry Point
+ *   * @size: Variable
+ *    * a: Variable
+ *     * b: Variable
+ *      * Return: No
+ *       */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	a = 0;
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
+	while (a < size)
+	{
+		b = 0;
+		while (b < size)
+		{
+			_putchar(35);
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
