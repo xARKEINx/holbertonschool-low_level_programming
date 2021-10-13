b851b9f1130b39e7fad23ff43d11c254549481e3
xARKEINx <3884@holbertonschool.com>
2021-10-13 12:05:03 -0700

    gusanos

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ * a: variable
+ * @n: variable
+ * Returne: none
+ */
+void print_line(int n)
+{
+
+	char a = 0;
+
+	while (n > a)
+	{
+		_putchar('_');
+		a++;
+	}
+	_putchar ('\n');
+
+}
