0dbf44ded555a73518a71f5f0e64ffd428c65240
xARKEINx <3884@holbertonschool.com>
2021-10-16 13:10:39 -0700

    array

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strlen - entry point
+ * @*s: variable
+ * i: variable
+ * Return: 0
  */
 int _strlen(char *s)
 {
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
