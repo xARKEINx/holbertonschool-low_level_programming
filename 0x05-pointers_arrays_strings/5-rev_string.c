5ce41e3235d992ab60c48e9a4d614d572c3195b7
xARKEINx <3884@holbertonschool.com>
2021-10-16 16:55:57 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *rev_string - entry point
+ *@s: string
+ *a: variable
+ *b: variable
+ *c: variable
+ */
+void rev_string(char *s)
+{
+	int a;
+	char b[100];
+	int c = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		b[a] = s[a];
+	}
+	for (a = a - 1; a >= 0; a--)
+	{
+		s[a] = b[c];
+		c++;
+	}
+}
