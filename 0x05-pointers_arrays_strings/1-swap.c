ce302b5858ef1c5f05548e94f75fcfd3064dd0ba
xARKEINx <3884@holbertonschool.com>
2021-10-15 18:17:11 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ * swap_int -entry point
+ * c: variable
+ * @a: variable
+ * @b: variable
  */
 void swap_int(int *a, int *b)
 {
 	int c;
-	
+
 	c = *a;
 	*a = *b;
 	*b = c;
