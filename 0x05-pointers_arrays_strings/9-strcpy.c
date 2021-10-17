fa29052c9dfa99e82afe4c41e6644a5b93673569
xARKEINx <3884@holbertonschool.com>
2021-10-17 14:49:18 -0700

    problem

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - entry point
+ * @dest: Variable
+ * @src: variable
+ * a: variable
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+	
+	for (a = 0; src[a] != 0; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = src[a];
+	return (dest);
+}
