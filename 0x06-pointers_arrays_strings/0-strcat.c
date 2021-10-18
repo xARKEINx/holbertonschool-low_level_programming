a006477c32d4e76286a094a9e5e258a726ba61ed
xARKEINx <3884@holbertonschool.com>
2021-10-18 15:27:48 -0700

    el nulo limpia

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,25 @@
 #include"main.h"
 /**
- *
+ * _strcat - entry point
+ * a: variable
+ * b: variable
+ * Return: pointer dest
+ * @dest: variable
+ * @src: variable
  */
 char *_strcat(char *dest, char *src)
 {
 	int a;
 	int b;
 
-	for(a = 0; dest[a] != '\0'; a++)
+	for (a = 0; dest[a] != '\0'; a++)
 		;
 	for (b = 0; src[b] != '\0'; b++)
 	{
 		dest[a] = src[b];
 		a++;
 	}
-	dest[a]='\0';
+	dest[a] = '\0';
 
-	return(dest);
+	return (dest);
 }
