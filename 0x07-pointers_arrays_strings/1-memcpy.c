f52215ccd8c54a010c2a300f1a2a59db71d33c4f
xARKEINx <3884@holbertonschool.com>
2021-10-22 20:29:16 -0700

    ezz

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,11 @@
 #include "main.h"
 /**
- *
+ * _memcpy - entry point
+ * @dest: variable
+ * @src: variable
+ * @n: variable
+ * i: variable
+ * Return: pointer dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
