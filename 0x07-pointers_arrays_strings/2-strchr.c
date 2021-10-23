11f2419020f5feaa589269c78457d3d5d0f9ee6e
xARKEINx <3884@holbertonschool.com>
2021-10-22 20:50:13 -0700

    return

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - entry point
+ * a: variable
+ * @s: variable
+ * @c: variable
+ * Return: s + 2
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int a;
+
+	for (a = 0; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+		{
+			return (s + a);
+		}
+	}
+	return (NULL);
+}
