bab72f6ec222706aa961e2d87f00ba56c613087e
xARKEINx <3884@holbertonschool.com>
2021-10-24 00:27:22 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - entry point
+ * @n: variable
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n < 2)
+	{
+		return (n);
+	}
+	else
+	{
+	return (n * factorial(n - 1));
+	}
+}
