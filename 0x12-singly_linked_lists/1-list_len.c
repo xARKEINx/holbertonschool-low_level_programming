b3be6cda555bcef911cc4a2263796ed4ae447367
xARKEINx <3884@holbertonschool.com>
2021-11-18 16:16:23 -0800

    all

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,18 @@
 #include "lists.h"
-
 /**
- *  * list_len - print number of elements
- *   * @h: Pointer to head
- *    * n: My variable
- *     * Return: n
- *      */
-
+ * list_len - print number of elements
+ * @h: Pointer to head
+ * n: My variable
+ * Return: n
+ */
 size_t list_len(const list_t *h)
 {
-		size_t n = 0;
+	size_t n = 0;
 
-			while (h != NULL)
-					{
-								n++;
-										h = h->next;
-											}
-				return (n);
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
