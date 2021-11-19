2e011ef6459f092c7d1491eb5b4327cc37919625
xARKEINx <3884@holbertonschool.com>
2021-11-18 16:24:23 -0800

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,33 +1,27 @@
 #include "lists.h"
-
 /**
- *  * add_node - add new node
- *   * @head: Pointer to HeadPointer
- *    * @str: String
- *     * new_node: New node
- *      * i: Iterator
- *       * Return: head
- *        */
-
+ * add_node - add new node
+ * @head: Pointer to HeadPointer
+ * @str: String
+ * new_node: New node
+ * i: Iterator
+ * Return: head
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-		list_t *new_node;
-			int i;
+	list_t *new_node;
+	int i;
 
-				new_node = malloc(sizeof(list_t));
-
-					if (new_node == NULL)
-							{
-										return (NULL);
-											}
-
-						for (i = 0; str[i] != '\0'; i++)
-									;
-
-							new_node->str = strdup(str);
-								new_node->len = i;
-									new_node->next = *head;
-										*head = new_node;
-
-											return (*head);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	new_node->str = strdup(str);
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
 }
