9077cdb8d174b6fcc9e483d58b2070e170486901
xARKEINx <3884@holbertonschool.com>
2021-11-18 16:42:05 -0800

    extra

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,25 +1,23 @@
 #include "lists.h"
-
 /**
- *  * free_list - Function free list
- *   * @head: Pointer
- *    * node: variable
- *     * release: Variable
- *      * Return: No line
- *       */
-
+ * free_list - Function free list
+ * @head: Pointer
+ * node: variable
+ * release: Variable
+ * Return: No line
+ */
 void free_list(list_t *head)
 {
-		list_t *node;
-			list_t *release;
+	list_t *node;
+	list_t *release;
 
-				node = head;
+	node = head;
 
-					while (node != NULL)
-							{
-										release = node->next;
-												free(node->str);
-														free(node);
-																node = release;
-																	}
+	while (node != NULL)
+	{
+		release = node->next;
+		free(node->str);
+		free(node);
+		node = release;
+	}
 }
